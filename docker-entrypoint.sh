89237f31e3a839331131b153b3fe11fac7ef9366
Nguoi An Phu <nguoianphu@gmail.com>
2016-12-26 17:33:03 +0700

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,8 @@ set -xe
 # If user don't provide any command
 # Run filebeat
 if [[ "$1" == "" ]]; then
-     tail -f /opt/stack/logs/stack.sh.log
+     # tail -f /opt/stack/logs/stack.sh.log
+     /opt/devstack/stack.sh
 else
     # Else allow the user to run arbitrarily commands like bash
     exec "$@"
